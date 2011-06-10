c6eda85683dc395884fa35cff24d97ea97e6d722
jewilmeer <jewilmeer@gmail.com>
2011-06-10 17:10:51 +0200

    changed port to env port

--- socket.io.js
+++ socket.io.js
@@ -7,4 +7,7 @@ var server = http.createServer(function(req, res) {
   res.end('<h1>Hello world</h1>');
 })
 
-server.listen(8888);
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+  console.log('server running on port ' + port)
+});
