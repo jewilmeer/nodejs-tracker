0acb0a5c221dfb96a34474a047032f15138f8809
jewilmeer <jewilmeer@gmail.com>
2011-06-10 17:12:39 +0200

    created a simple chat server

--- socket.io.js
+++ socket.io.js
@@ -1,10 +1,35 @@
-var http = require('http')
-var io   = require('socket.io')
+// var http = require('http')
+// var io   = require('socket.io')
+// 
+// var server = http.createServer(function(req, res) {
+//   // server code here...
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('<h1>Hello world</h1>');
+// })
+// 
+// var port = process.env.PORT || 3000;
+// server.listen(port, function() {
+//   console.log('server running on port ' + port)
+// });
 
-var server = http.createServer(function(req, res) {
-  // server code here...
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Hello world</h1>');
+var net = require('net')
+
+var sockets = []
+
+var s = net.Server(function(socket) {
+  sockets.push( socket )
+  
+  socket.on('data', function(data) {
+    for( var i=0; i < sockets.length; i++ ) {
+      sockets[i].write(data);
+    }
+  })
+  
+  socket.on('end', function() {
+    console.log("We\'ve got a quitter");
+    var i = sockets.indexOf( socket );
+    sockets.splice(i, 1);
+  })
 })
 
 var port = process.env.PORT || 3000;
